94b0abb1edfdec1559516a8085eafe57fcae686b
ZekeZR1 <hnta3574@gmail.com>
2019-01-23 12:32:22 +0900

    Sprite supports 3D cameras

--- ZekeGame/ZekeGame/Game/StageSetup/StageSetup.cpp
+++ ZekeGame/ZekeGame/Game/StageSetup/StageSetup.cpp
@@ -4,6 +4,8 @@
 #include "../Monster/Monster.h"
 #include "../Monster/Monsters/TestMons.h"
 #include "../Monster/Monsters/Uma.h"
+#include "../SaveLoad/PythonFileLoad.h"
+#include <string>
 #include <array>
 
 StageSetup::StageSetup()
@@ -59,9 +61,10 @@ void StageSetup::PVPSetup(std::vector<std::string> files, int monsterAI[6],Monst
 }
 
 
-void StageSetup::DungeonSetup(std::vector<std::string> files, int monsterAI[6], MonsterID monids[6], int DunNumber) {
+void StageSetup::DungeonSetup(PyFile files, PyFile eneFiles, int monsterAI[6], MonsterID monids[6], int dunNumber) {
 	int team = 0;
 	int num = 0;
+
 	CVector3 poss[6];
 	poss[0] = { 250,0,500 };
 	poss[1] = { 0,0,500 };
@@ -70,10 +73,12 @@ void StageSetup::DungeonSetup(std::vector<std::string> files, int monsterAI[6], 
 	poss[4] = { 0,0,-500 };
 	poss[5] = { -250,0,-500 };
 
+	SetEnemyAI(dunNumber,monsterAI, monids);
+
 	for (int i = 0; i < 6; i++)
 	{
 		if (i == 3)
-			team++;
+			team = 256;
 
 		Monster* mon = nullptr;
 		switch (monids[i])
@@ -88,13 +93,46 @@ void StageSetup::DungeonSetup(std::vector<std::string> files, int monsterAI[6], 
 		mon->Setpos(poss[i]);
 		mon->Setnum(i);
 		mon->Setteam(team);
-		std::string* path = new std::string("PythonAIs.");
-		//std::string* path = new std::string("");
-		*path += files[monsterAI[i]];
-		mon->SetpyFile(path->c_str());
+		if (team) {
+			//team
+			std::string* path = new std::string("PythonAIs.");
+			//std::string* path = new std::string("");
+			*path += files[monsterAI[i]];
+			mon->SetpyFile(path->c_str());
+		}
+		else{
+			//enemy
+			std::string* path = new std::string("PythonEnemyAIs.");
+			*path += eneFiles[monsterAI[i]];
+			mon->SetpyFile(path->c_str());
+		}
 		g_mons[i] = mon;
 	}
 	g_buddyCount = 3;
 	g_enemyCount = 3;
 	g_monsCount = 6;
+}
+
+//TODO set AI
+void StageSetup::SetEnemyAI(int dun, int* monAI, MonsterID* monId) {
+	switch (dun) {
+	case 0:
+		monAI[3] = 1;
+		monId[3] = enTest;
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	}
 }
