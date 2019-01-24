9de2c21fa84f75eff2a8fa9e738985e5322c116d
ZekeZR1 <hnta3574@gmail.com>
2019-01-24 14:14:31 +0900

    add dungeon result

--- ZekeGame/ZekeGame/Game/Dungeon/DungeonSelect.h
+++ ZekeGame/ZekeGame/Game/Dungeon/DungeonSelect.h
@@ -1,5 +1,7 @@
 #pragma once
 
+class GameCursor;
+
 class DungeonSelect : public GameObject
 {
 public:
--- ZekeGame/ZekeGame/Game/Result/DungeonResult.h
+++ ZekeGame/ZekeGame/Game/Result/DungeonResult.h
@@ -3,10 +3,14 @@
 class DungeonResult :public GameObject
 {
 public:
+	bool Start() override;
+	void Update() override;
+	void OnDestroy() override;
 	void init(int team)
 	{
 		m_team = team;
 	}
 private:
 	int m_team = 0;
+	SpriteRender* m_resultSp = nullptr;
 };
