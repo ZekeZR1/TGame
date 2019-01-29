6fff2e7edf277905493920defde03b4ffcea5354
ZekeZR1 <hnta3574@gmail.com>
2019-01-29 14:12:59 +0900

    add dungeon clear save func

--- ZekeGame/ZekeGame/TestScene.cpp
+++ ZekeGame/ZekeGame/TestScene.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <fstream>
 #include "GameCamera.h"
 #include "Game/GameCamera2D.h"
 #include "Game/GameCursor.h"
@@ -27,7 +28,32 @@ bool TestScene::Start() {
 	ppp.z -= 3.f;
 	m_camera->SetPosition(ppp);
 	m_camera->Update();
+	{
+		using namespace std;
+		ofstream fout;
+		double a;
+		fout.open("datafile.txt", ios::out | ios::binary | ios::trunc);
+		a = 0;
+		fout.write((char*)&a, sizeof(double));
+		fout.close();
+	}
+	{
+		using namespace std;
+		char outfile[] = "datafile.txt";  
 
+		ifstream fin(outfile, ios::in | ios::binary);
+		if (!fin) {
+			OutputDebugStringA("datafileÇÃÉIÅ[ÉvÉìÇ…é∏îsÇµÇ‹ÇµÇΩ");
+			return 1;
+		}
+		double d;  
+
+		fin.read((char *)&d, sizeof(double));
+		char message[256];
+		sprintf_s(message,"%f\n",d);
+		OutputDebugStringA(message);
+		fin.close();  
+	}
 	m_boneModel = NewGO<SkinModelRender>(0, "model");
 	m_anim[0].Load(L"Assets/modelData/uma/anim_uma_atack.tka");
 	m_anim[0].SetLoopFlag(true);
