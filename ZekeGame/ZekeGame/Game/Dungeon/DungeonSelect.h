74e9e9e1bf1595d4e642daf1631594a6393a3d3e
ZekeZR1 <hnta3574@gmail.com>
2019-01-30 14:29:41 +0900

    add back button to dun select

--- ZekeGame/ZekeGame/Game/Dungeon/DungeonSelect.h
+++ ZekeGame/ZekeGame/Game/Dungeon/DungeonSelect.h
@@ -12,14 +12,17 @@ public:
 	void OnDestroy() override;
 private:
 	void CheckDungeonClearState();
+	void InitDungeonButtons();
 	int m_clearedDunNum = -1;
 	std::map<SpriteRender*, int> m_dungeonButton;
 	std::vector<SpriteRender*> m_sps;
 	std::vector<FontRender*> m_fonts;
 	SpriteRender* m_nextSp = nullptr;
+	SpriteRender* m_backSp = nullptr;
 	CVector2 m_fPivot = { 0.5f,0.5f };
 	GameCursor* m_cur;
 	CVector3 m_pos;
+	FontRender* m_backTx = nullptr;
 	const CVector4 m_notYetClearCol = { 0.5,0.5f,0.5f,1.f };
 	const CVector4 m_ClearCol = CVector4::White;
 	const int m_numDungeon = 8;
