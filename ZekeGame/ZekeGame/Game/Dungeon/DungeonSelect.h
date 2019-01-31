009f155d50589c052a30119b455a887d905714e5
ZekeZR1 <hnta3574@gmail.com>
2019-01-31 15:35:44 +0900

    add easing func and fix dun sel ui

--- ZekeGame/ZekeGame/Game/Dungeon/DungeonSelect.h
+++ ZekeGame/ZekeGame/Game/Dungeon/DungeonSelect.h
@@ -5,31 +5,37 @@ class GameCursor;
 class DungeonSelect : public GameObject
 {
 public:
-	DungeonSelect();
-	~DungeonSelect();
 	bool Start() override;
 	void Update() override;
 	void OnDestroy() override;
 private:
 	void CheckDungeonClearState();
 	void InitDungeonButtons();
+	void InitSideButtons();
+	void InitBackButton();
+	void PositionUpdate();
+	void NextSpAnimation();
+	void DungeonSelectClick();
+	void StartDungeon();
 	int m_clearedDunNum = -1;
 	std::map<SpriteRender*, int> m_dungeonButton;
 	std::vector<SpriteRender*> m_sps;
 	std::vector<FontRender*> m_fonts;
 	SpriteRender* m_nextSp = nullptr;
 	SpriteRender* m_backSp = nullptr;
+	SpriteRender* m_rightSp = nullptr;
+	SpriteRender* m_leftSp = nullptr;
 	CVector2 m_fPivot = { 0.5f,0.5f };
 	GameCursor* m_cur;
-	CVector3 m_pos;
 	FontRender* m_backTx = nullptr;
+	CVector3 addPos = CVector3::Zero();
 	const CVector4 m_notYetClearCol = { 0.5,0.5f,0.5f,1.f };
 	const CVector4 m_ClearCol = CVector4::White;
 	const int m_numDungeon = 8;
-	const float m_spaceToNextSprite = 80.f;
-	const float SPRITE_W = 350.f;
-	const float SPRITE_H  = 70.f;
+	const float m_spaceToNextSprite = 800.f;
+	const float SPRITE_W = 600.f;
+	const float SPRITE_H  = 600.f;
 	const CVector2 m_toFixMisalignment = { -150.f, 20.f };
-	int count = 0;
+	bool m_isPositionUpdating = false;
 };
 
---
+++ ZekeGame/ZekeGame/Game/Dungeon/DungeonTransition.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "DungeonTransition.h"
+
+
+DungeonTransition::DungeonTransition()
+{
+}
+
+
+DungeonTransition::~DungeonTransition()
+{
+}
+
+bool DungeonTransition::Start() {
+	return true;
+}
+
+void DungeonTransition::OnDestroy() {
+
+}
+
+void DungeonTransition::Update() {
+
+}
+
