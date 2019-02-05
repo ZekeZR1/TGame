c1fc129bc8045dea3f9245b8af92cad744779975
ZekeZR1 <hnta3574@gmail.com>
2019-02-05 11:23:57 +0900

    fix effect

--- ZekeGame/ZekeGame/Engine/graphics/Effect/CEffect.cpp
+++ ZekeGame/ZekeGame/Engine/graphics/Effect/CEffect.cpp
@@ -15,16 +15,24 @@ bool CEffect::Start() {
 }
 
 void CEffect::Update() {
-	g_graphicsEngine->GetEffectEngine().GetEffekseerManager().SetScale(m_handle, m_scale.x, m_scale.y, m_scale.z);
-	Effekseer::Matrix43 mBase;
-	CMatrix mRot, mScale, mTrans;
+	//g_graphicsEngine->GetEffectEngine().GetEffekseerManager().SetScale(m_handle, m_scale.x, m_scale.y, m_scale.z);
+	//Effekseer::Matrix43 mBase;
+	//CMatrix mRot, mScale, mTrans;
+	//mTrans.MakeTranslation(m_position);
+	//mRot.MakeRotationFromQuaternion(m_rotation);
+	//mScale.MakeScaling(m_scale);
+	//Effekseer::Matrix43::Multiple(mBase, mScale, mRot);
+	//Effekseer::Matrix43::Multiple(mBase, mBase, mTrans);
+	//g_graphicsEngine->GetEffectEngine().GetEffekseerManager().SetBaseMatrix(m_handle, mBase);
+	CMatrix mTrans, mRot, mScale, mBase;
 	mTrans.MakeTranslation(m_position);
 	mRot.MakeRotationFromQuaternion(m_rotation);
 	mScale.MakeScaling(m_scale);
-	Effekseer::Matrix43::Multiple(mBase, mScale, mRot);
-	Effekseer::Matrix43::Multiple(mBase, mBase, mTrans);
+	mBase = mScale * mRot;
+	mBase = mBase * mTrans;
 	g_graphicsEngine->GetEffectEngine().GetEffekseerManager().SetBaseMatrix(m_handle, mBase);
-	if (!IsPlay()) {
+	if (!IsPlay() && isPlayed) {
+		Stop();
 		DeleteGO(this);
 	}
 }
@@ -36,6 +44,7 @@ void CEffect::Render() {
 void CEffect::Play(const wchar_t* filepath) {
 	m_effect = Effekseer::Effect::Create(&(g_graphicsEngine->GetEffectEngine().GetEffekseerManager()), (const EFK_CHAR*)filepath);
 	m_handle = g_graphicsEngine->GetEffectEngine().GetEffekseerManager().Play(m_effect, m_position.x, m_position.y, m_position.z);
+	isPlayed = true;
 }
 
 void CEffect::Stop() {
