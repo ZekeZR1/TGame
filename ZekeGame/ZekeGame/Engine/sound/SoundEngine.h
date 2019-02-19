3bc891cb5cf0605f57aa6554d97e85d3cb87afca
Zeke <hnta3574@gmail.com>
2019-02-19 17:53:42 +0900

    fix sound

--- ZekeGame/ZekeGame/Engine/sound/SoundEngine.h
+++ ZekeGame/ZekeGame/Engine/sound/SoundEngine.h
@@ -1,17 +1,16 @@
 #pragma once
-class Sound {
+
+class SoundEngine {
 public:
-	Sound();
-	~Sound();
+	SoundEngine();
+	~SoundEngine();
 	void Update();
-	void Init(const wchar_t*, bool);
-	void Play();
-	bool isPlaying();
-	void Play(bool);
-	void Stop();
+//private:
 	std::unique_ptr<DirectX::AudioEngine> audEngine;
-	std::unique_ptr<DirectX::SoundEffect> soundEffect;
-	std::unique_ptr<DirectX::SoundEffectInstance> effect;
 };
 
+static SoundEngine& ISoundEngine() {
+	static SoundEngine soundEngine;
+	return soundEngine;
+}
 
