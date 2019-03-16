b562e77d4b9835e6a5c5ac66f2c3dadd3d01d30a
Zeke <hnta3574@gmail.com>
2019-03-17 00:29:40 +0900

    add monster box class

---
+++ ZekeGame/ZekeGame/Game/MonsterBox/MonsterBox.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "MonsterBox.h"
+
+
+MonsterBox::MonsterBox()
+{
+}
+
+
+MonsterBox::~MonsterBox()
+{
+}
+
+bool MonsterBox::Start() {
+	return true;
+}
+
+void MonsterBox::Update() {
+
+}
---
+++ ZekeGame/ZekeGame/Game/MonsterBox/MonsterBox.h
@@ -0,0 +1,13 @@
+#pragma once
+class MonsterBox : public GameObject
+{
+public:
+	MonsterBox();
+	~MonsterBox();
+	bool Start();
+	void Update();
+	//void OnDestroy();
+private:
+
+};
+
