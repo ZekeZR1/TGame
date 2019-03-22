a9d7600ae6912f9d82f94551fb2eb074b588a6dc
ZekeZR1 <hnta3574@gmail.com>
2019-03-22 16:49:38 +0900

    add monster drop performance

--- ZekeGame/ZekeGame/Game/Dungeon/MonsterDrop.cpp
+++ ZekeGame/ZekeGame/Game/Dungeon/MonsterDrop.cpp
@@ -20,51 +20,17 @@ MonsterDrop::~MonsterDrop()
 
 
 bool MonsterDrop::Start() {
-	m_egg = NewGO<SkinModelRender>(0);
-	m_egg->Init(L"Assets/modelData/egg.cmo");
-	m_back = NewGO<SkinModelRender>(0);
-	m_back->Init(L"Assets/modelData/dun_yuka.cmo");
 	std::random_device rnd;
 	auto drop = rnd() % 100;
 	//if (drop >= 50) 
 		//ToDungeonSelect();
-	camera3d->SetPosition({ 0.f,300.f,600.f });
+	camera3d->SetPosition({ 0.f,100.f,450.f });
 	camera3d->Update();
-	m_monsterNameSp = NewGO<SpriteRender>(0);
-	m_nextButtonSp = NewGO<SpriteRender>(0);
-	m_nextButtonSp->Init(L"Assets/Sprite/button1.dds", 300.f, 100.f, true);
-	m_nextButtonSp->SetPosition(m_nextButtonPos);
-	m_cursor = NewGO<GameCursor>(0);
+	InitUI();
+	InitModels();
 	m_monsterId = static_cast<MonsterID>(rnd() % enNumMonster);
-	m_notifySp = NewGO<SpriteRender>(0);
-	m_notifySp->Init(L"Assets/Sprite/notifyLine.dds", 650.f, 50.f);
-	m_notifySp->SetPosition(m_notifyLinePos);
-	m_notifyFont = NewGO<FontRender>(0);
-	m_notifyFont->SetTextType(CFont::en_Japanese);
 	IMonsterBox().GetMonster(m_monsterId);
-	m_monster = NewGO<SkinModelRender>(0);
-	switch (m_monsterId) {
-	case enTest:
-		//ToDungeonSelect();
-		//m_monsterSp->Init(L"Assets/Sprite/fade_black.dds", 500.f, 500.f);
-		//break;
-	case enUmataur:
-		m_animClip[0].Load(L"Assets/modelData/uma/anim_uma_idle.tka");
-		m_animClip[0].SetLoopFlag(true);
-		m_monster->Init(L"Assets/modelData/uma.cmo", m_animClip, 1);
-		m_monsterNameSp->Init(L"Assets/Sprite/.dds",300.f,100.f);
-		m_notifyFont->Init(L"”n‚¿‚á‚ñ‚ðƒQƒbƒg‚µ‚½‚æ", { m_notifyLinePos.x - 270.f,m_notifyLinePos.y + 15.f }, 0.f, CVector4::White, 1.f, { 1.f,1.f });
-		break;
-	case enFairy:
-		m_animClip[0].Load(L"Assets/modelData/fairy/hnd_idle.tka");
-		m_animClip[0].SetLoopFlag(true);
-		m_monster->Init(L"Assets/modelData/hnd.cmo", m_animClip, 1);
-		m_monsterNameSp->Init(L"Assets/Sprite/.dds", 300.f, 100.f);
-		m_notifyFont->Init(L"—d¸‚¿‚á‚ñ‚ðƒQƒbƒg‚µ‚½‚æ", { m_notifyLinePos.x - 370.f,m_notifyLinePos.y + 15.f }, 0.f, CVector4::White, 1.f, { 1.f,1.f });
-
-		break;
-	}
-	m_monster->PlayAnimation(0);
+	//NewMonster();
 	m_fade = FindGO<Fade>("fade");
 	m_fade->FadeIn();
 	return true;
@@ -83,6 +49,11 @@ void MonsterDrop::OnDestroy() {
 
 void MonsterDrop::Update() {
 	SceneTransition();
+	EggPfm();
+	if (g_pad[0].IsTrigger(enButtonA)) {
+		DeleteGO(this);
+		NewGO<MonsterDrop>(0);
+	}
 }
 
 
@@ -105,5 +76,102 @@ void MonsterDrop::SceneTransition() {
 	}
 	if (m_fade->isFadeStop() && m_fadeFlag) {
 		ToDungeonSelect();
+	}
+}
+
+void MonsterDrop::NewMonster() {
+	m_monster = NewGO<SkinModelRender>(0);
+	m_notifySp = NewGO<SpriteRender>(0);
+	m_notifySp->Init(L"Assets/Sprite/notifyLine.dds", 650.f, 50.f);
+	m_notifySp->SetPosition(m_notifyLinePos);
+	switch (m_monsterId) {
+	case enTest:
+		//ToDungeonSelect();
+		//m_monsterSp->Init(L"Assets/Sprite/fade_black.dds", 500.f, 500.f);
+		//break;
+	case enUmataur:
+		m_animClip[0].Load(L"Assets/modelData/uma/anim_uma_idle.tka");
+		m_animClip[0].SetLoopFlag(true);
+		m_monster->Init(L"Assets/modelData/uma.cmo", m_animClip, 1);
+		//m_monsterNameSp->Init(L"Assets/Sprite/.dds", 300.f, 100.f);
+		m_notifyFont->Init(L"”n‚¿‚á‚ñ‚ðƒQƒbƒg‚µ‚½‚æ", { m_notifyLinePos.x - 270.f,m_notifyLinePos.y + 15.f }, 0.f, CVector4::White, 1.f, { 1.f,1.f });
+		break;
+	case enFairy:
+		m_animClip[0].Load(L"Assets/modelData/fairy/hnd_idle.tka");
+		m_animClip[0].SetLoopFlag(true);
+		m_monster->Init(L"Assets/modelData/hnd.cmo", m_animClip, 1);
+		//m_monsterNameSp->Init(L"Assets/Sprite/.dds", 300.f, 100.f);
+		m_notifyFont->Init(L"—d¸‚¿‚á‚ñ‚ðƒQƒbƒg‚µ‚½‚æ", { m_notifyLinePos.x - 370.f,m_notifyLinePos.y + 15.f }, 0.f, CVector4::White, 1.f, { 1.f,1.f });
+		break;
+	}
+	m_monster->PlayAnimation(0);
+	m_monster->SetPosition(m_monsterPos);
+}
+
+void MonsterDrop::InitUI() {
+	m_cursor = NewGO<GameCursor>(0);
+	//m_monsterNameSp = NewGO<SpriteRender>(0);
+	m_nextButtonSp = NewGO<SpriteRender>(0);
+	m_nextButtonSp->Init(L"Assets/Sprite/button1.dds", 300.f, 100.f, true);
+	m_nextButtonSp->SetPosition(m_nextButtonPos);
+	m_notifyFont = NewGO<FontRender>(1);
+	m_notifyFont->SetTextType(CFont::en_Japanese);
+}
+
+void MonsterDrop::InitModels() {
+	m_egg = NewGO<SkinModelRender>(0);
+	m_egg->Init(L"Assets/modelData/egg.cmo", nullptr, 0, enFbxUpAxisY);
+	m_back = NewGO<SkinModelRender>(0);
+	m_back->Init(L"Assets/modelData/dropwall.cmo");
+	m_back->SetPosition(m_backPosition);
+	CQuaternion rot = CQuaternion::Identity();
+	rot.SetRotationDeg(CVector3::AxisX(), 90.f);
+	m_back->SetRotation(rot);
+}
+
+void MonsterDrop::EggPfm() {
+	if (m_egg == nullptr)
+		return;
+	CVector3 eggMove = CVector3::Zero();
+	m_eggPos += eggMove;
+	if (m_timer <= 2.f) {
+		m_timer += 0.15f;
+	}
+	else {
+		m_timer = 0.f;
+		m_eggnum++;
+	}
+	float ease = EASE::InOutQuad(4.f, -2.f, 2.f, m_timer);
+	CQuaternion rot = CQuaternion::Identity();
+	rot.SetRotationDeg(CVector3::AxisZ(), ease * m_eggnum);
+	m_eggRot.Multiply(rot);
+	m_egg->SetPosition(m_eggPos);
+	m_egg->SetRotation(m_eggRot);
+	if (m_eggnum == 5.f) {
+		if (!mb_efk) {
+			m_efk = NewGO<CEffect>(0);
+			m_efk->SetScale({ 6.f,6.f,6.f });
+			m_efk->Play(L"Assets/effect/dropefk.efk", 2.5f);
+			mb_efk = true;
+		}
+	}
+	if (m_eggnum >= 5) {
+		//big
+		float eggsize = 1.2f;
+		if (mf_eggSca >= eggsize) {
+			mflag_eggbig = false;
+		}
+		if (mf_eggSca <= eggsize && mflag_eggbig) {
+			mf_eggSca += 0.1f;
+		}
+		if(mf_eggSca >= 0.0f && !mflag_eggbig)
+			mf_eggSca -= 0.1f;
+		m_eggSca *= mf_eggSca;
+		m_egg->SetScale(m_eggSca);
+	}
+	if (m_eggnum == 14.f) {
+		DeleteGO(m_egg);
+		m_egg = nullptr;
+		NewMonster();
 	}
 }
