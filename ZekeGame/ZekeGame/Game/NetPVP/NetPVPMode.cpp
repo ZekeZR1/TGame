ddc98904dbfa4690cee1a6531a6ab1eaf899ac64
ZekeZR1 <hnta3574@gmail.com>
2019-04-16 11:25:20 +0900

    delete some dead code

--- ZekeGame/ZekeGame/Game/NetPVP/NetPVPMode.cpp
+++ ZekeGame/ZekeGame/Game/NetPVP/NetPVPMode.cpp
@@ -50,11 +50,6 @@ void NetPVPMode::init(std::vector<std::string> files, int monai[3], int moid[3])
 		m_monai[i] = monai[i];
 		m_moid[i] = moid[i];
 	}
-	//test
-	//for (int i = 3; i < 6; i++) {
-	//	m_monai[i] = 0;
-	//	m_moid[i] = 2;
-	//}
 }
 
 bool NetPVPMode::Start() {
@@ -75,7 +70,7 @@ void NetPVPMode::Update() {
 	 char str[256];
 	 int onlinePlayerNum = Engine::IEngine().GetNetworkLogic()->GetLBL()->GetOnlinePlayerCount();
 	 sprintf_s(str, "active online user num is %d\n", onlinePlayerNum);
-	 OutputDebugString(str);
+	 //OutputDebugString(str);
 	 auto lbl = Engine::IEngine().GetNetworkLogic()->GetLBL();
 	 RaiseData();
 	 LoadEnemyData();
@@ -88,67 +83,24 @@ void NetPVPMode::Update() {
 		 }
 		 BattleStart();
 	 }
-//#if _DEBUG
-//	if (g_pad[0].IsTrigger(enButtonA))
-//		RaiseData();
-//#endif
-//	//m_isConect = m_exdata->isConect();
-//	//if (m_isConect) {
-//	//	//OutputDebugString("Matching!!!!!! \n");
-//	//}
-//	//if (!m_isConect) {
-//	//	//OutputDebugString("WAITING \n");
-//	//	return;
-//	//}
-//	if (Engine::IEngine().GetNetworkLogic()->GetLBL()->GetOnlinePlayerCount() != 2) {
-//		char str[256];
-//		sprintf_s(str, "active online user num is %d\n", Engine::IEngine().GetNetworkLogic()->GetLBL()->GetOnlinePlayerCount());
-//		OutputDebugString(str);
-//		return;
-//	}
-//	m_informationSp->Init(L"Assets/Sprite/ready.dds", 300.f, 50.f);
-//	RaiseData();
-//	LoadEnemyData();
-//	if (m_dataLoaded && m_dataRaised) {
-//		for (int i = 3; i < 6; i++) {
-//			m_monai[i] = 0;
-//			m_moid[i] = m_enemyId[i - 3];
-//		}
-//		BattleStart();
-//	}
 }
 
 void NetPVPMode::RaiseData() {
-	//if (m_dataRaised)
-		//return;
-	//MonsterData
+	//Raise Monster ids
 	int ids[3];
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++) 
 		ids[i] = m_moid[i];
-		char str[256];
-		sprintf_s(str, "raise id is %d\n", ids[i]);
-		//OutputDebugString(str);
-	}
 	auto lbl = Engine::IEngine().GetNetworkLogic()->GetLBL();
 	lbl->SetTeamMonsterInfo(ids);
 	lbl->raiseMonData();
-	//m_dataRaised = true;
-	//Engine::IEngine().GetNetworkLogic()->GetLBL()->SetTeamMonsterInfo(ids);
-	/*for (int i = 0; i < 3; i++) {
-		m_exdata->sendMonData(i, m_monai[i]);
-		Engine::IEngine().GetNetworkLogic()->GetLBL()->raiseMonData();
-		OutputDebugString("raise monster data\n");
-	}*/
-	//Monster Code
+	//Raise Monster AIs
 
-	//if(raiseMonData){
 }
 
 void NetPVPMode::LoadEnemyData() {
 	if (m_dataLoaded) 
 		return;
-	//if (!Engine::IEngine().GetNetworkLogic()->GetLBL()->isHang())
-		//return;
+	//Load Enemy Ids
 	auto ids = Engine::IEngine().GetNetworkLogic()->GetLBL()->GetEnemyTeamIDs();
 	if (ids[0] == 0)
 		return;
@@ -156,18 +108,9 @@ void NetPVPMode::LoadEnemyData() {
 		m_enemyId[i] = ids[i];
 		OutputDebugString("LOADING ENEMY TEAM MONSTER ID DATAS\n");
 	}
+	//Load Enemy AIs
+
 	m_dataLoaded = true;
-	/*int num = Engine::IEngine().GetNetworkLogic()->GetLBL()->GetMonNum();
-	int id = Engine::IEngine().GetNetworkLogic()->GetLBL()->GetMonID();
-	char str[256];
-	sprintf_s(str, "Monster NUM is %d !!!!!! MOOONnster ID is %d\n", num, id);
-	OutputDebugString(str);
-	m_enemyId[num] = id;
-	if (m_enemyId[0] != -1 &&
-		m_enemyId[1] != -1 &&
-		m_enemyId[2] != -1) {*/
-		//m_dataLoaded = true;
-	//}
 }
 
 void NetPVPMode::BattleStart() {
