c24aa988f2f31d966314b37acfd64c6cde5549d4
kokoasann <kokoasann@gmail.com>
2019-05-07 15:18:08 +0900

    i

--- ZekeGame/ZekeGame/Game/StageSetup/StageSetup.cpp
+++ ZekeGame/ZekeGame/Game/StageSetup/StageSetup.cpp
@@ -421,6 +421,7 @@ void StageSetup::NetworkPvPSetup(std::vector<std::string> files,
 
 		mon = GameData::LoadMonster(monids[i]);
 		if (team == 0) {
+			//My team data
 			if (aimode[i] == 0) {
 				path = new std::string("PythonAIs.");
 				*path += files[i];
@@ -432,8 +433,18 @@ void StageSetup::NetworkPvPSetup(std::vector<std::string> files,
 			}
 		}
 		else {
-			path = new std::string("NetworkEnemyAIs.");
-			*path += enemyFiles[i-3];
+			//Enemy team data
+			if (aimode[i] == 0) {
+				path = new std::string("NetworkEnemyAIs.");
+				*path += enemyFiles[i - 3];
+			}
+			else {
+				path = new std::string("NetworkEnemyAIs/");
+				//*path += files[monsterAI[i]];
+				*path += static_cast<char>(i);
+				*path += "enemy.va";
+				mon->SetVisualScriptAI(path);
+			}
 		}
 		mon->Setpos(poss[i]);
 		mon->Setnum(i);
@@ -441,7 +452,7 @@ void StageSetup::NetworkPvPSetup(std::vector<std::string> files,
 
 		mon->SetpyFile(path);
 		g_mons[i] = mon;
-
+		//delete path;
 	}
 
 	g_buddyCount = 3;
