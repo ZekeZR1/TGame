c05c1195f2a936d98973f1889d4d89bf8b4a182e
ZekeZR1 <hnta3574@gmail.com>
2019-06-17 16:42:17 +0900

    技一覧表示のバグを修正

--- ZekeGame/ZekeGame/Game/AIEdit/ShowMonsterSkills.cpp
+++ ZekeGame/ZekeGame/Game/AIEdit/ShowMonsterSkills.cpp
@@ -1,10 +1,18 @@
 #include "stdafx.h"
 #include "ShowMonsterSkills.h"
-
+#include "ShowMonsters.h"
+#include "../GameCursor.h"
 
 bool ShowMonsterSkills::Start() {
 	m_backSp = NewGO<SpriteRender>(3);
 	m_backSp->Init(L"Assets/sprite/menu.dds", 600, 720);
+
+	m_quitSp = NewGO<SpriteRender>(10);
+	m_quitSp->Init(L"Assets/sprite/deletepoint.dds", 50, 50,true,false);
+	m_quitSp->SetPosition({ 280,330,0 });
+
+	m_cur = FindGO<GameCursor>("cursor");
+
 	static const int Ydist = 110;
 	CVector3 pos = { -200, 250, 0 };
 	for (int i = 0; i < 6 ; i++) {
@@ -22,9 +30,17 @@ bool ShowMonsterSkills::Start() {
 }
 
 void ShowMonsterSkills::OnDestroy() {
+	for (auto sp : m_sps)
+		DeleteGO(sp);
 	DeleteGO(m_backSp);
+	DeleteGO(m_quitSp);
 }
 
 void ShowMonsterSkills::Update() {
-
+	m_quitSp->SetCollisionTarget(m_cur->GetCursor());
+	if (m_quitSp->isCollidingTarget() and Mouse::isTrigger(enLeftClick)) {
+		auto smc = FindGO<ShowMonsters>("MonstersInfo");
+		smc->SetWindowActive(true);
+		DeleteGO(this);
+	}
 }
