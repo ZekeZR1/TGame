8f2e1fd750ecb692f375822664f02e7b828ae1b1
ZekeZR1 <hnta3574@gmail.com>
2019-06-27 17:20:59 +0900

    musicがない場合は何もしない

--- ZekeGame/ZekeGame/PythonEnemyAIs/_MiddleExecute.py
+++ ZekeGame/ZekeGame/PythonEnemyAIs/_MiddleExecute.py
@@ -2,9 +2,25 @@
 import importlib
 
 def execute(num,team,file):
-    mod = importlib.import_module(file)
     try:
+        mod = importlib.import_module(file)
         mod.Brain(num,team)
     except:
-        pass
+        import sys
+        import os
+        from datetime import datetime
+
+        path = __file__
+        path = path[:path.rfind("\\")] + "/error.log"
+        with open(path,"a") as file:
+            etuple = sys.exc_info()
+            file.write("---ERROR---\n")
+            time = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            file.write(time+"\n")
+            file.write("MonsterNum:"+str(num)+"\n")
+            file.write("MonsterTeam:"+str(team)+"\n")
+            file.write(str(etuple[0])+"\n")
+            file.write(str(etuple[1])+"\n")
+            file.write(str(etuple[2])+"\n\n")
+        return 0
     return 1
