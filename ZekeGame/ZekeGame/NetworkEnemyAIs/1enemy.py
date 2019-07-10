97c8833cc2de4824ecac13511f3847ce0ca9e619
kokoasann <kokoasann@gmail.com>
2019-07-10 17:06:41 +0900

    cmo

--- ZekeGame/ZekeGame/NetworkEnemyAIs/1enemy.py
+++ ZekeGame/ZekeGame/NetworkEnemyAIs/1enemy.py
@@ -1,27 +1,12 @@
 ﻿#coding:utf-8
 
-import PythonAIs.CppBridge as cb
+import CppBridge as cb
 
-def Brain(num,team):
-    cb.init(num,team)
+def Brain():
+    cb.Init()
     me = cb.GetMe()
-    tar = cb.GetEnemyLowHPMonster()
-    if me.ID == cb.SendGame.Goblin:
-        cb.SendGame.addAction(tar.num,cb.SendGame.ATK_Debuff)
-        cb.SendGame.addAction(tar.num,cb.SendGame.Poison)
-        cb.SendGame.addAction(tar.num,cb.SendGame.ATTACK)
-        pass
-    elif me.ID == cb.SendGame.Kikyo:
-        cb.SendGame.addAction(tar.num,cb.SendGame.SUPER_BEAM)
-        lm = cb.GetBuddyLowMPMonster()
-        cb.SendGame.addAction(lm.num,cb.SendGame.MP_Heal)
-        pass
-    elif me.ID == cb.SendGame.RedHead:
-        if len(cb.gameData.Enemys) != 0:
-            cb.SendGame.addAction(tar.num,cb.SendGame.Thunder)
-            cb.SendGame.addAction(tar.num,cb.SendGame.Ignite)
-        else:
-            cb.SendGame.addAction(0,cb.SendGame.ESCAPE)
-            cb.SendGame.addAction(tar.num,cb.SendGame.Thunder)
-        pass
+    pos = me.position
+    pos.x += 300
+    pos.z += 1000
+    cb.SendGame.Move(pos.x,pos.z)
     cb.End()
